fd88ee312457de91ec2161bd8f69e757cf30ece3
mehedi1256 <mehedihassanshovospirt@gmail.com>
2023-02-20 22:43:50 +0600

    2nd commit

--- js/jquery.blurr.js
+++ js/jquery.blurr.js
@@ -5,7 +5,7 @@
  *  --
  *  Released under the MIT Licence
  * 
-    Copyright (c) 2014 Tom Hallam
+    Copyright (c) 2022 Tom Hallam
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
